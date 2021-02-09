c944f95bcd8a00fa671ea96a6e2df1fe89ba76a3
Jan Friebe <friebe@odas.biz>
2021-02-09 20:02:11 +0100

    chore: fix error on build command

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './styles/main.postcss'
-import routes from 'vite-plugin-pages/client'
+import routes from 'pages-generated'
 import { ViteSSG } from 'vite-ssg'
 import App from './App.vue'
+
 
 // https://github.com/antfu/vite-ssg
 export const createApp = ViteSSG(
