a383f61a7a635fdb6af81d48b4bff99abbeb2147
Jan Friebe <friebe@odas.biz>
2021-02-12 19:00:14 +0100

    fix:typo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="mt-10 mb-6 prose m-auto opacity-50 flex">
-    <span class="text-sm">© 2021, Built with <a target="_blank" rel="noopener" href="https://vitejs.dev/" style="color:inherit">Vite</a> and love and hosted by <a target="_blank" rel="noopener" href="https://www.netlify.com/" style="color:inherit">netlify</a></span>
+    <span class="text-sm">© 2021, Built with <a target="_blank" rel="noopener" href="https://vitejs.dev/" style="color:inherit">Vite</a> much love and hosted by <a target="_blank" rel="noopener" href="https://www.netlify.com/" style="color:inherit">netlify</a></span>
   </footer>
 </template>
